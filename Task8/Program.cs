c205b1042c4fbc745eb71cbe9117d8b3d9330e73
Aleksey Nesterenko <naa1205@bk.ru>
2022-08-31 11:32:12 +0700

    Task8 fixed againd

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Введите число: ");
 int number1 = int.Parse(Console.ReadLine ());
 int number = 1;
 Console.Write($"Четные числа от 1 до {number1}: ");
-while(number<number1 + 1) {
+while(number <= number1) {
     if(number % 2 == 0) 
     {
     Console.Write($"{number} ");
