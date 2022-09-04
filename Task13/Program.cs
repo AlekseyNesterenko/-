e095d3707e9a3de15bde08dabf38d55df7ea8891
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-05 02:17:32 +0700

    Начало задания 13

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void DelSecDigit ()
+    {
+        Console.Write("Введите целое число: ");
+        double num = int.Parse(Console.ReadLine ());
+        if (x<100) 
+        {
+           Console.WriteLine("В числе нет третьей цифры."); 
+        }
+        else
+        {
+        int count = 0;
+        while(x>0)
+        {
+        num = num/10; 
+        count++;
+        }
+        }
+       double DigitsCout = Math.Pow(10, count);
+       double 
+        
+    }
+
+
+DelSecDigit();
