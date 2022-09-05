d9bdcee004ac10a9b3df660d66a1d167f25e1081
AlekseyNesterenko <112189834+AlekseyNesterenko@users.noreply.github.com>
2022-09-05 18:33:38 +0700

    Fixed after evaluation

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("Введите целое число: ");
 int num = int.Parse(Console.ReadLine ());
 
-void ThirdDigit ()
+void getThirdDigit ()
     {
         num = Math.Abs(num);
         if (num>=0 && num<100) 
@@ -12,15 +12,13 @@ void ThirdDigit ()
         }
         else
         {
-        while(num>999)
-            {
-            num = num/10; 
-            }
-        int num1 = num % 10;
-        Console.WriteLine($"Третья цифра в числе: {num1}"); 
+            while(num>999)
+                 {
+                     num = num/10; 
+                 }
+            int num1 = num % 10;
+            Console.WriteLine($"Третья цифра в числе: {num1}"); 
         }
     }
 
-ThirdDigit();
-
-
+getThirdDigit();
