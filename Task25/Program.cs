9ed9c1cf5c39739060f21ee784e4c009951007d2
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-12 14:55:30 +0700

    Задание 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,30 +1,35 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Решить задачу с использованием методов.
 
-int GetPowerOfNumber (int a, int b)
+void GetPowerOfNumber (int a, int b)
 {
     int powerOfNum = a;
-    if (a == 0 && b>0) powerOfNum = 0;
-    if (a !=0 && b == 0) powerOfNum = 1;
+    if (a !=0 && b == 0)
+    {
+      powerOfNum = 1;
+      Console.WriteLine($"{a} в степни {b} = {powerOfNum}");
+    } 
+    
     if (b>0)
     {
         for (int i =1; i<b; i++)
         {
           powerOfNum = powerOfNum*a;
         }
+        Console.WriteLine($"{a} в степни {b} = {powerOfNum}");
     }
+
     if (b<0)
     {
        Console.WriteLine("Степень числа должна быть натуральным числом!");  
     }
-    return powerOfNum;
 }
 
-Console.Write("Input A: ");
+Console.Write("Основание: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Input B: ");
+Console.Write("Степень: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-int result = GetPowerOfNumber(numberA,numberB);
-Console.WriteLine($"{numberA} в степни {numberB} = {result}");
+GetPowerOfNumber(numberA,numberB);
+
 
