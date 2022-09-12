5734797cfe1f7748c4476629815d4c5bb586635c
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-12 13:48:35 +0700

    Задание 27,29

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int getSumNumberDigits(int number)
+{
+       int sum=0;
+       int count = 0;
+       int tempNumber = Math.Abs(number);
+        while (tempNumber > 0)
+        {
+            int lastDigit = tempNumber % 10;
+            sum = sum + lastDigit;
+            count++;
+        }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число: ");
+try
+{
+    int a = int.Parse(Console.ReadLine());
+    int digitsSum = getSumNumberDigits(a);
+    Console.WriteLine($"Сумма цифр в числе: {digitsSum}");
+}
+catch (Exception)
+{
+    Console.WriteLine($"Входное значение имеет неверный формат");
+}
--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся пользователем.
+
+int[] InputArray()
+{
+    int[] array = new int[8];
+    Console.WriteLine("Введите элементы массива: ");
+    for (int i=0;i<array.Length; i++)
+    {
+        try
+        {
+            Console.Write($"Элемент массива с индексом [{i}]= ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            System.Console.WriteLine("Входное значение имеет неверный формат");
+            i--;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Массив из 8 элементов: ");
+    for( int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] array = InputArray();
+Console.WriteLine();
+PrintArray(array);
