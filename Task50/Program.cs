12b015c75913c875c335fa7785424a6a4a722ea8
Aleksey Nesterenko <naa1205@bk.ru>
2022-09-23 16:51:53 +0700

    Продолжение дз 7

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -2,23 +2,57 @@
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 
-double[,] CreateArray(int m, int n)
+int[,] CreateArray(int m, int n)
 {
-    double[,] array = new double[m, n];
+    int[,] array = new int[m, n];
     var random = new Random();
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i, j] = Math.Round(random.NextDouble() * 200-100, 1);  // рандомные вещественные числа от -100 до 100 с округением до 1 знака.
+            array[i, j] = random.Next(-100, 101);
         }
     }
     return array;
 }
 
+void FindArrayElement(int[,] array, int m, int n)
+{
+    if (m > array.GetLength(0) | n > array.GetLength(1))
+    {
+        Console.WriteLine("Такого элемента нет в массиве");
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if( m == array.GetLength(0) && n == array.GetLength(1))
+                {
+                Console.WriteLine($"Элемент с иденксом [{m},{n}] равен {array[i, j]}");
+                }
+            }
+            break;
+        }
+    }
+}
 
-int m = 0; // количество строк
-int n = 0; // количество столбцов
+int rows = new Random().Next(2, 11);
+int columns = new Random().Next(2, 11);
+int[,] Array = CreateArray(rows, columns);
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{Array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int m = 0; // строка
+int n = 0; // столбец
 
 Console.Write("Введите индекс (m): ");
 bool parseMIsOk = int.TryParse(Console.ReadLine(), out int resultM);
@@ -44,15 +78,5 @@ else
     n = resultN;
 }
 
-double[,] resultArray = CreateArray (m, n);
-for(int i =0; i<m; i++)
-{
-    for(int j = 0; j< n; j++)
-    {
-        Console.Write($"{resultArray[i,j]} ");
-    }
-    Console.WriteLine();
-}
-
-
+FindArrayElement(Array, m, n);
 
