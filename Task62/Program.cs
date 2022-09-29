90cb6f569a47aac3228cc16b309c1692357430b7
AlekseyNesterenko <112189834+AlekseyNesterenko@users.noreply.github.com>
2022-09-29 23:31:03 +0700

    Задание 62

--- Task62/Program.cs
+++ Task62/Program.cs
@@ -1,3 +1,34 @@
 ﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
 
+int[,] FillArraySpiral()
+{
+  int[,] array = new int[4,4];
+  int i = 0;
+  int j = 0;
+  int count = 0;
+   while (count < array.GetLength(0) * array.GetLength(1))
+   {
+     if(j<array.GetLength(1)) 
+     {
+       array[i, j] = count++;
+       j++;
+     }
+     if (i<array.GetLength(0))
+        {
+       array[i, j] = count++;
+       i++;
+     }
+         if(j<array.GetLength(1)) 
+     {
+       array[i, j] = count++;
+       j--;
+     }
+     if (i<array.GetLength(0))
+        {
+       array[i, j] = count++;
+       i--;
+     }
+     
+   }
+}
 
